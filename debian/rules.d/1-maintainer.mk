ce02a44e2008705fbf7ba1b83a157df3d6a3a781
Ian May <ianm@nvidia.com>
2025-04-14 12:24:42 -0700

    UBUNTU: Ubuntu-nvidia-adv-6.11-6.11.0-2002.2
    Signed-off-by: Ian May <ianm@nvidia.com>

--- debian/rules.d/1-maintainer.mk
+++ debian/rules.d/1-maintainer.mk
@@ -102,7 +102,7 @@ printchanges:
 		baseCommit=$$(git log --pretty=format:'%H %s' | \
 			gawk '/UBUNTU:\s*Ubuntu-.*$$/ { print $$1; exit }'); \
 	fi; \
-	git log "$$baseCommit"..HEAD | \
+	git log --no-color "$$baseCommit"..HEAD | \
 	$(DROOT)/scripts/misc/git-ubuntu-log
 
 .PHONY: insertchanges
